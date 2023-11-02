ad317ce7129f6ecc5065c257ff86da8c5624ac81
zhour <zhourongjun1234@outlook.com>
2023-11-02 13:32:52 +0800

    登录功能

--- src/main/java/cn/edu/njupt/forum/conf/WebMvcConfig.java
+++ src/main/java/cn/edu/njupt/forum/conf/WebMvcConfig.java
@@ -3,15 +3,20 @@ package cn.edu.njupt.forum.conf;
 import cn.edu.njupt.forum.interceptor.AuthInterceptor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
+    private final HandlerMethodArgumentResolver userInfoArgumentResolver;
 
-    public WebMvcConfig(AuthInterceptor authInterceptor) {
+    public WebMvcConfig(AuthInterceptor authInterceptor, HandlerMethodArgumentResolver userInfoArgumentResolver) {
         this.authInterceptor = authInterceptor;
+        this.userInfoArgumentResolver = userInfoArgumentResolver;
     }
 
     @Override
@@ -19,5 +24,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login", "register");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userInfoArgumentResolver);
     }
 }
