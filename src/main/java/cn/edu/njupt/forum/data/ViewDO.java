8fc83642a2789f96ba82500404545dff2a5d56af
zhour <zhourongjun1234@outlook.com>
2023-11-03 09:53:35 +0800

    feat:历史记录

---
+++ src/main/java/cn/edu/njupt/forum/data/ViewDO.java
@@ -0,0 +1,11 @@
+package cn.edu.njupt.forum.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViewDO {
+}
